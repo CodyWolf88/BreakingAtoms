16a627859c0a73eed84b82f99b7760264ff48408
CodyWolf <77299915+CodyWolf88@users.noreply.github.com>
2026-03-14 16:51:17 +0200

    Book system update

--- Assets/Core/cele mai cool scripturi ever/BookClick.cs
+++ Assets/Core/cele mai cool scripturi ever/BookClick.cs
@@ -1,15 +1,20 @@
+using System;
 using System.Linq;
 using UnityEngine;
 
 public class BookClick : MonoBehaviour
 {
     public GameObject bookUI; // Drag your UI Panel here
-
+    
     void OnMouseDown()
     {
         bookUI.SetActive(true);
         Debug.Log("Book was clicked!"); // This will show up in the 'Console' tab
     }
 
+    private void Start()
+    {
+        bookUI.SetActive(false);
+    }
 }
 
--- Assets/Core/cele mai cool scripturi ever/BookSystem.cs
+++ Assets/Core/cele mai cool scripturi ever/BookSystem.cs
@@ -1,5 +1,8 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class BookSystem : MonoBehaviour
 {
@@ -7,13 +10,22 @@ public class BookSystem : MonoBehaviour
     public Image pageDisplay; // The UI Image component showing the page
     public AudioClip[] pageTurnSounds; // Drag your 6 sounds here
 
+    public Button firstPageNextButton;
+    public Button nextButton;
+    public Button previousButton;
+    
     private int currentPage = 0;
     private AudioSource audioSource;
+    
+    
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         UpdatePage();
+        
+        nextButton.gameObject.SetActive(false);
+        previousButton.gameObject.SetActive(false);
     }
 
     public void NextPage()
@@ -24,11 +36,41 @@ public class BookSystem : MonoBehaviour
             UpdatePage();
             PlayRandomSound();
         }
+
+    }
+    
+    public void PreviousPage()
+    {
+        if (currentPage > 0)
+        {
+            currentPage--;
+            UpdatePage();
+            PlayRandomSound();
+        }
     }
 
     void UpdatePage()
     {
         pageDisplay.sprite = pages[currentPage];
+
+        if (currentPage == 0)
+        {
+            firstPageNextButton.gameObject.SetActive(true);
+            nextButton.gameObject.SetActive(false);
+            previousButton.gameObject.SetActive(false);
+        }
+        else if (currentPage == pages.Length - 1)
+        {
+            firstPageNextButton.gameObject.SetActive(false);
+            nextButton.gameObject.SetActive(false);
+            previousButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            firstPageNextButton.gameObject.SetActive(false);
+            nextButton.gameObject.SetActive(true);
+            previousButton.gameObject.SetActive(true);
+        }
     }
 
     void PlayRandomSound()
@@ -39,4 +81,5 @@ public class BookSystem : MonoBehaviour
 
     public void OpenBook() => gameObject.SetActive(true);
     public void CloseBook() => gameObject.SetActive(false);
+    
 }
