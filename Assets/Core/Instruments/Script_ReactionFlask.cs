d5f64f8876663b103acc79e9cc51682ffd1fb9ab
CodyWolf <77299915+CodyWolf88@users.noreply.github.com>
2026-03-14 15:17:04 +0200

    Flask

--- Assets/Core/Instruments/Script_ReactionFlask.cs
+++ Assets/Core/Instruments/Script_ReactionFlask.cs
@@ -2,6 +2,16 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using TMPro;
+
+public enum ReactionCondition
+{
+    Normal,
+    Heat,
+    Cold,
+    Electricity
+}
+
 public class Script_ReactionFlask : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -10,29 +20,38 @@ public class Script_ReactionFlask : MonoBehaviour
     {
         public string recipeName;
         public List<string> requiredSymbols;
+
+        public ReactionCondition reactionCondition = ReactionCondition.Normal;
     }
+
+    [SerializeField]
+    private TextMeshProUGUI elementsText;
     
     [Header("Recipes")]
     public List<Recipe> allRecipes;
     
     [Header("Elements in flask")]
     public List<string> elementsInFlask;
-
+    
+    public ReactionCondition currentCondition = ReactionCondition.Normal;
     private void CheckReactionFlask()
     {
         elementsInFlask.Sort();
         string currentSignature = string.Join(",", elementsInFlask);
+        
+        string elementsIn_Text = string.Join("+", elementsInFlask);
+        elementsText.text = elementsIn_Text;
 
         foreach (Recipe recipe in allRecipes)
         {
             recipe.requiredSymbols.Sort();
             string recipeSignature = string.Join(",", recipe.requiredSymbols);
 
-            if (currentSignature == recipeSignature)
+            if (currentSignature == recipeSignature && currentCondition == recipe.reactionCondition)
             {
-                elementsInFlask.Clear();
+                //elementsInFlask.Clear();
                 
-                Debug.Log("Table salt");
+                Debug.Log(recipe.recipeName);
                 return;
             }
         }
